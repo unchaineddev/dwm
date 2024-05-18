2f96e000052a7e7a8d82d44375bfb75d28dfa94d
yusuf <yusuftechie+git@gmail.com>
2024-05-18 08:17:01 +0530

    changed icons

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 
 
 // Theme 
-#include "/home/yusuf/dev/git/suckless/dwm/themes/moonfly.h"
+#include "/home/yusuf/dev/git/suckless/dwm/themes/red.h"
 
 //static const char *brupcmd[] = { "brightnessctl", "set", "10%+", NULL };
 //static const char *brdowncmd[] = { "brightnessctl", "set", "10%-", NULL };
@@ -43,7 +43,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -84,7 +84,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *firefox[]  = { "firefox", NULL };
 static const char *brave[]  = { "brave", NULL };
 static const char *thunderbird[] = { "thunderbird", NULL};
